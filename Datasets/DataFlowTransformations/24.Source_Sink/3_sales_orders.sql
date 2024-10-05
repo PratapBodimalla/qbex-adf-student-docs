68ff1ec28bcac8489531e5eecb7d8fe72cb6f692
Pratap Reddy Bodimalla <PratapBodimalla>
2024-10-05 12:18:10 +0530

    added datasets for transformations

---
+++ Datasets/DataFlowTransformations/24.Source_Sink/3_sales_orders.sql
@@ -0,0 +1,45 @@
+CREATE TABLE SalesData (
+    SalesOrder VARCHAR(50),
+    OrderDate DATE,
+    CustomerName VARCHAR(100),
+    City VARCHAR(100),
+    Country VARCHAR(50),
+    Product VARCHAR(100),
+    Price INT,
+    Qty_Sold INT,
+    Qty_Sold_Units VARCHAR(20),
+    Amount INT,
+    Amount_Currency VARCHAR(10)
+);
+
+
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO101', '2024-09-8', 'Amit Joshi', 'Hyderabad', 'India', 'Keyboard', 3000, 4, 'pieces', 12000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO102', '2024-09-19', 'Divya Reddy', 'Mumbai', 'India', 'Printer', 1500, 5, 'pieces', 7500, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO103', '2024-09-1', 'Rohan Patel', 'Pune', 'India', 'Laptop', 1500, 10, 'pieces', 15000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO104', '2024-09-30', 'Divya Reddy', 'Pune', 'India', 'External Hard Drive', 10000, 8, 'pieces', 80000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO105', '2024-09-14', 'Nikhil Desai', 'Ahmedabad', 'India', 'Wireless Mouse', 8000, 10, 'pieces', 80000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO106', '2024-09-20', 'Pooja Mehta', 'Hyderabad', 'India', 'Monitor', 8000, 10, 'pieces', 80000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO107', '2024-09-10', 'Amit Joshi', 'Mumbai', 'India', 'Webcam', 10000, 2, 'pieces', 20000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO108', '2024-09-3', 'Nikhil Desai', 'Pune', 'India', 'Wireless Mouse', 8000, 1, 'pieces', 8000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO109', '2024-09-3', 'Amit Joshi', 'Ahmedabad', 'India', 'Monitor', 70000, 5, 'pieces', 350000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1010', '2024-09-1', 'Nikhil Desai', 'Ahmedabad', 'India', 'Monitor', 2000, 7, 'pieces', 14000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1011', '2024-09-19', 'Amit Joshi', 'Chennai', 'India', 'Printer', 2000, 1, 'pieces', 2000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1012', '2024-09-8', 'Rohan Patel', 'Mumbai', 'India', 'Keyboard', 3000, 5, 'pieces', 15000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1013', '2024-09-14', 'Nikhil Desai', 'Mumbai', 'India', 'Keyboard', 10000, 10, 'pieces', 100000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1014', '2024-09-2', 'Pooja Mehta', 'Ahmedabad', 'India', 'Laptop', 10000, 10, 'pieces', 100000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1015', '2024-09-23', 'Amit Joshi', 'Chennai', 'India', 'Laptop', 8000, 6, 'pieces', 48000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1016', '2024-09-20', 'Nikhil Desai', 'Ahmedabad', 'India', 'Webcam', 8000, 4, 'pieces', 32000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1017', '2024-09-9', 'Nikhil Desai', 'Mumbai', 'India', 'Keyboard', 8000, 6, 'pieces', 48000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1018', '2024-09-26', 'Nikhil Desai', 'Pune', 'India', 'Laptop', 10000, 5, 'pieces', 50000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1019', '2024-09-18', 'Rohan Patel', 'Mumbai', 'India', 'Keyboard', 2000, 5, 'pieces', 10000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1020', '2024-09-24', 'Amit Joshi', 'Pune', 'India', 'External Hard Drive', 5000, 5, 'pieces', 25000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1021', '2024-09-16', 'Amit Joshi', 'Mumbai', 'India', 'Webcam', 10000, 7, 'pieces', 70000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1022', '2024-09-30', 'Amit Joshi', 'Pune', 'India', 'Laptop', 70000, 5, 'pieces', 350000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1023', '2024-09-13', 'Nikhil Desai', 'Mumbai', 'India', 'Wireless Mouse', 3000, 3, 'pieces', 9000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1024', '2024-09-8', 'Pooja Mehta', 'Ahmedabad', 'India', 'Wireless Mouse', 5000, 2, 'pieces', 10000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1025', '2024-09-25', 'Rohan Patel', 'Chennai', 'India', 'External Hard Drive', 5000, 7, 'pieces', 35000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1026', '2024-09-14', 'Divya Reddy', 'Ahmedabad', 'India', 'Keyboard', 1500, 8, 'pieces', 12000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1027', '2024-09-24', 'Rohan Patel', 'Hyderabad', 'India', 'Laptop', 3000, 9, 'pieces', 27000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1028', '2024-09-25', 'Rohan Patel', 'Chennai', 'India', 'Monitor', 5000, 8, 'pieces', 40000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1029', '2024-09-17', 'Divya Reddy', 'Chennai', 'India', 'Wireless Mouse', 1500, 8, 'pieces', 12000, 'INR');
+INSERT INTO SalesData (SalesOrder, OrderDate, CustomerName, City, Country, Product, Price, Qty_Sold, Qty_Sold_Units, Amount, Amount_Currency) VALUES ('SO1030', '2024-09-8', 'Nikhil Desai', 'Chennai', 'India', 'Webcam', 8000, 9, 'pieces', 72000, 'INR');
